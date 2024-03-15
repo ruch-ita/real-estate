a96f029aba52a1702a5b3940f8c3e97dceec8642
ruch-ita <rathodruchita10@gmail.com>
2024-03-15 17:52:33 +0530

    CREATE AND RUN THE SERVER

---
+++ api/index.js
@@ -0,0 +1,11 @@
+import { log } from 'console';
+import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect("")
+
+const app = express();
+
+app.listen(3000, () =>{
+    console.log("server is running on port 3000");
+});
