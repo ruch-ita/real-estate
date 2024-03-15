dce0036a142a8e9518fde3033f99bbff17ab1513
ruch-ita <rathodruchita10@gmail.com>
2024-03-15 15:41:19 +0530

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 function App() {
   return <BrowserRouter>
+  <Header/>
   <Routes>
     <Route path="/home" element={<Home />} />
     <Route path="/sign-in" element={<SignIn />} />
