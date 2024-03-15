26150a66ffb259fb980300a73bcb3c9c8c64251f
ruch-ita <rathodruchita10@gmail.com>
2024-03-16 00:45:43 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,11 +1,21 @@
-import { log } from 'console';
-import express from 'express';
-import mongoose from 'mongoose';
+import { log } from "console";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 
-mongoose.connect("")
+const dbConnect = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO)
+    console.log("Database Connected");
+  } catch (error) {
+    console.log("ERROR WHILE CONNECTING TO DATABASE");
+  }
+};
 
+dbConnect();
 const app = express();
 
-app.listen(3000, () =>{
-    console.log("server is running on port 3000");
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
 });
