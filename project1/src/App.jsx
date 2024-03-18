5a539faa94e61fcd74cfc906e15dd9a4c73b866b
ruch-ita <rathodruchita10@gmail.com>
2024-03-18 17:30:09 +0530

    create sign up page UI

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,7 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import SignnUp from './pages/SignnUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
@@ -11,7 +11,7 @@ function App() {
   <Routes>
     <Route path="/home" element={<Home />} />
     <Route path="/sign-in" element={<SignIn />} />
-    <Route path="/sign-up" element={<SignUp />} />
+    <Route path="/sign-up" element={<SignnUp />} />
     <Route path="/about" element={<About />} />
     <Route path="/profile" element={<Profile />} />
   </Routes>
