21f65d37209376f49805175087babbffc8d2b3c0
ruch-ita <rathodruchita10@gmail.com>
2024-03-18 15:51:38 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRouter from './router/user.route.js';
+import userRouter from './route/user.route.js';
+import authRouter from './route/auth.route.js';
+
 dotenv.config();
 
 const dbConnect = async () => {
@@ -17,11 +20,14 @@ const dbConnect = async () => {
 dbConnect();
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
 
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 
